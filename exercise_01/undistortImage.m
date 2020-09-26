aee9f7e5d0155189d62f3697c158da93143d7147
Beat Hubmann <BeatHubmann@users.noreply.github.com>
2020-09-26 17:18:18 +0200

    Add comment

--- exercise_01/undistortImage.m
+++ exercise_01/undistortImage.m
@@ -6,7 +6,7 @@ function img_undistorted = undistortImage(img, K, D)
 % OUT:  Undistorted image
 
 [height, width] = size(img); % Get image dimensions
-num_pixels = height * width;
+num_pixels = height * width; % Total pixel count
 
 [X, Y] = meshgrid(1:width, 1:height); % Set up meshgrid to define pixels
 pixels = [X(:)-1, Y(:)-1, ones(num_pixels, 1)]'; % Pixel matrix zero-based [3 x num_pixels]
@@ -17,7 +17,7 @@ pixels_normalized_distorted = [pixels_normalized_distorted;
                                ones(1, num_pixels)]; % Augment z coordinate
 pixels_distorted = K * pixels_normalized_distorted; % Reverse normalization
 pixels_distorted = pixels_distorted(1:2, :); % Remove z coordinate augmentation
-pixels_distorted_integer = round(pixels_distorted); % Round pixel coordinates to nearest integer values
-pixel_grayscales = img(height*pixels_distorted_integer(1, :) + pixels_distorted_integer(2, :)); % Read out pixel greyscale values at coordinates
-img_undistorted = uint8(reshape(pixel_grayscales, [height, width])); % Reshape pixel values into image shape, convert to 256-bit
+pixels_distorted_integer = round(pixels_distorted); % Round pixel coordinates to nearest integer values for nearest-neighbor interpolation
+pixel_grayscales = img(height*pixels_distorted_integer(1, :) + pixels_distorted_integer(2, :)); % Read out pixel greyscale values at NN-coordinates
+img_undistorted = uint8(reshape(pixel_grayscales, [height, width])); % Reshape NN pixel values into image shape, convert to 256-bit greyscale
 end
