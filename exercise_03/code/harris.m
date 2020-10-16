221235d19c4a73654276e3e5f2a8392afcd394b5
Beat Hubmann <BeatHubmann@users.noreply.github.com>
2020-10-16 15:50:09 +0200

    Complete ex03, part 1

--- exercise_03/code/harris.m
+++ exercise_03/code/harris.m
@@ -1,1 +1,41 @@
 function scores = harris(img, patch_size, kappa)
+% Calculates the Harris score for feature detection on given image
+% IN:   Grayscale imput image: img [h x w]
+% IN:   Scalar size of square patch in one dimension, odd number: patch_size
+% IN:   Scalar 'magic number' hyperparameter in (0.04, 0.15): kappa
+% OUT:  Corner response function values of img: scores [h x w]
+
+% assemble Sobel filter
+sobel = [1, 2, 1]' * [-1, 0, 1];
+
+% img free of noise => apply Sobel filter without smoothing,
+% obtain image first partial derivatives
+Ix = conv2(img, sobel, 'valid');
+Iy = conv2(img, sobel', 'valid');
+
+% obtain pixel-wise products of gradients
+Ixx = Ix .^ 2;
+Iyy = Iy .^ 2;
+Ixy = Ix .* Iy;
+
+% assemble box filter
+box = ones(patch_size, patch_size);
+
+% apply box filter
+sum_Ixx = conv2(Ixx, box, 'valid');
+sum_Iyy = conv2(Iyy, box, 'valid');
+sum_Ixy = conv2(Ixy, box, 'valid');
+
+% Harris: R = det(M) - kappa*trace(M)^2
+detM = sum_Ixx .* sum_Iyy - sum_Ixy .^ 2;
+traceM = sum_Ixx + sum_Iyy;
+scores = detM - kappa * traceM .^2;
+
+% set negative scores to zero
+scores(scores<0) = 0;
+
+% pad score array to make sure score is returned for each pixel
+patch_radius = floor(patch_size / 2);
+padsize = [1+patch_radius 1+patch_radius];
+scores = padarray(scores, padsize);
+
--- exercise_03/code/shi_tomasi.m
+++ exercise_03/code/shi_tomasi.m
@@ -1,1 +1,48 @@
 function scores = shi_tomasi(img, patch_size)
+% Calculates the Shi-Tomasi score for feature detection on given image
+% IN:   Grayscale imput image: img [h x w]
+% IN:   Scalar size of square patch in one dimension, odd number: patch_size
+% OUT:  Corner response function values of img: scores [h x w]
+
+% assemble Sobel filter
+sobel = [1, 2, 1]' * [-1, 0, 1];
+
+% img free of noise => apply Sobel filter without smoothing,
+% obtain image first partial derivatives
+Ix = conv2(img, sobel, 'valid');
+Iy = conv2(img, sobel', 'valid');
+
+% obtain pixel-wise products of gradients
+Ixx = Ix .^ 2;
+Iyy = Iy .^ 2;
+Ixy = Ix .* Iy;
+
+% assemble box filter
+box = ones(patch_size, patch_size);
+
+% apply box filter
+sum_Ixx = conv2(Ixx, box, 'valid');
+sum_Iyy = conv2(Iyy, box, 'valid');
+sum_Ixy = conv2(Ixy, box, 'valid');
+
+% Shi-Tomasi: R = min(lambda1, lambda2) > treshold
+% to find lower eigenvalue pixel-wise, use that
+% for M = [M11 M12]
+%         [M21 M22]
+% lambda1, lambda2 = ((M11+M22)+-sqrt((M11-M22).^2 + 4*M12.^2))/2
+% where lambda1 >= lambda2
+scores = ((sum_Ixx+sum_Iyy) - sqrt((sum_Ixx-sum_Iyy).^2 + 4*sum_Ixy.^2))/2;
+
+% set negative scores to zero
+scores(scores<0) = 0;
+
+% pad score array to make sure score is returned for each pixel
+patch_radius = floor(patch_size / 2);
+padsize = [1+patch_radius 1+patch_radius];
+scores = padarray(scores, padsize);
+
+
+
+
+
+
